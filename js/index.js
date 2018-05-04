a1c14024682bf54b9c77cb6c8aefb4015e20b62e
ybaravy <ybaravy@klika-tech.com>
2018-05-04 14:07:05 +0300

    Animate a Canvas Circle

--- js/index.js
+++ js/index.js
@@ -1,36 +1,60 @@
 
 var canvas, ctx;
-var rectangleX = 0;
-var direction = 1;
-var ctxWidth = 100;
+
+var ball = {
+  radius: 10,
+  x: 10,
+  y: 10,
+  color: "blue",
+  speedX: 1,
+  speedY: 1,
+}
 
 window.onload = function init() {
   canvas = document.querySelector('#myCanvas');
   ctx = canvas.getContext("2d");
-
-  // setTimeout(animateCanvas, 100);
-
-  requestAnimationFrame(animateCanvas);
-
+  animateCanvas();
 }
 
 function animateCanvas() {
-  ctx.save();
-  ctx.clearRect(0,0,canvas.width, canvas.height)
-  drawRectangle(rectangleX);
-  rectangleX += direction;
 
-  if ((rectangleX >= canvas.width - ctxWidth) || (rectangleX <= 0)) {
-    direction = -direction;
-  };
-  // setTimeout(animateCanvas, 100);
+  ctx.clearRect(0,0,canvas.width, canvas.height)
+  
+  drawFigure(ball);
+  
+  ball.x += ball.speedX;
+  ball.y += ball.speedY;
+
+  testCollisions(ball);
+
   requestAnimationFrame(animateCanvas);
+
+}
+
+function drawFigure(ball){
+  ctx.save();
+  ctx.translate(ball.x, ball.y)
+  ctx.fillStyle = ball.color;
+  ctx.beginPath();
+  ctx.arc(0, 0, ball.radius, 0, 2*Math.PI);
+  ctx.fill();
   ctx.restore();
 }
 
-function drawRectangle(rectangleX){
-  ctx.save();
-  ctx.fillStyle="red";
-  ctx.fillRect(rectangleX,10,ctxWidth,50);
-  ctx.restore();
+function testCollisions(ball){
+  if (ball.x + ball.radius >= canvas.width) {
+    ball.speedX = -ball.speedX;
+    ball.x = canvas.width - ball.radius;
+  } else if (ball.x - ball.radius < 0) {
+    ball.speedX = -ball.speedX;
+    ball.x = ball.radius;
+  }
+
+  if (ball.y + ball.radius >= canvas.height) {
+    ball.speedY = -ball.speedY;
+    ball.y = canvas.height - ball.radius;
+  } else if (ball.y - ball.radius < 0) {
+    ball.speedY = -ball.speedY;
+    ball.y = ball.radius;
+  }
 }
