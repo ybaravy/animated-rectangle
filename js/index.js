81ef3e4b765610a108815303cc6189bf9a9d1966
ybaravy <ybaravy@klika-tech.com>
2018-05-04 15:17:22 +0300

    Animate Multiple Canvas Circles

--- js/index.js
+++ js/index.js
@@ -1,18 +1,20 @@
 
-var canvas, ctx;
+var canvas, ctx, mousePos;
 
-var ball = {
-  radius: 10,
-  x: 10,
-  y: 10,
-  color: "blue",
-  speedX: 1,
-  speedY: 1,
-}
+var balls = [];
 
 window.onload = function init() {
   canvas = document.querySelector('#myCanvas');
   ctx = canvas.getContext("2d");
+  canvas.addEventListener("mousemove", (event) => {
+    var rect = canvas.getBoundingClientRect();
+    mousePos = {
+      x: event.clientX - rect.left,
+      y: event.clientY - rect.top
+    }
+  })
+  balls = createShape(2);
+
   animateCanvas();
 }
 
@@ -20,25 +22,48 @@ function animateCanvas() {
 
   ctx.clearRect(0,0,canvas.width, canvas.height)
   
-  drawFigure(ball);
-  
-  ball.x += ball.speedX;
-  ball.y += ball.speedY;
-
-  testCollisions(ball);
+  drawShape(balls);
+  moveBall(balls);  
 
   requestAnimationFrame(animateCanvas);
 
 }
 
-function drawFigure(ball){
-  ctx.save();
-  ctx.translate(ball.x, ball.y)
-  ctx.fillStyle = ball.color;
-  ctx.beginPath();
-  ctx.arc(0, 0, ball.radius, 0, 2*Math.PI);
-  ctx.fill();
-  ctx.restore();
+function createShape(n) {
+  var ballsArr = [];
+  for (var i=0; i < n; i++) {
+    ball = {
+      radius: 5+15*Math.random(),
+      x: canvas.width/2,
+      y: canvas.height/2,
+      color: 'grey',
+      speedX: 1+2*Math.random(),
+      speedY: 1+2*Math.random()
+    };
+    ballsArr.push(ball);
+  }
+  return ballsArr;
+}
+
+function drawShape(balls){
+  balls.forEach(function(ball) {
+    ctx.save();
+    ctx.translate(ball.x, ball.y)
+    ctx.fillStyle = ball.color;
+    ctx.beginPath();
+    ctx.arc(0, 0, ball.radius, 0, 2*Math.PI);
+    ctx.fill();
+    ctx.restore();
+  })
+}
+
+function moveBall(balls) {
+  balls.forEach(function(ball) {
+    ball.x += ball.speedX;
+    ball.y += ball.speedY;
+
+    testCollisions(ball); 
+  })
 }
 
 function testCollisions(ball){
